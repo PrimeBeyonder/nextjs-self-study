b5a468387d18c73d56750374b89e7d35cd6682bf
Andrew <thum14252@gmail.com>
2024-06-28 21:24:20 +0630

    starter

---
+++ dashboard/app/page.tsx
@@ -0,0 +1,11 @@
+import {ArrowRightIcon} from "heroicons/24/outline";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+     <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+     </div>
+     
+    </main>
+  );
+}
